b2a3ac8b13d54f946078f997ce271a4f7fbb6017
wcushen <54533890+wcushen@users.noreply.github.com>
2021-02-02 17:55:36 +1100

    Update script.py

--- script.py
+++ script.py
@@ -3,21 +3,44 @@ import os
 import time
 
 
-path = os.path.abspath("output")
-print(path)
-if not os.path.exists(path):
-    os.makedirs(path)
+# store API credentials
+with open('.secret/api_credentials.json', 'r') as f:
+    api_credentials = json.load(f)
+    client_id = api_credentials['client_id']
+    client_secret = api_credentials['client_secret']
+    refresh_token = api_credentials['refresh_token']
+# make POST request to Strava API
+req = requests.post("https://www.strava.com/oauth/token?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token".format(client_id, client_secret, refresh_token)).json()
+# update API credentials file
+api_credentials['access_token'] = req['access_token']
+api_credentials['refresh_token'] = req['refresh_token']
+with open('.secret/api_credentials.json', 'w') as f:
+    json.dump(api_credentials, f)
+# store new access token
+access_token = api_credentials['access_token']
+
+with open('request_log.csv', 'r') as f:
+    # read file line-by-line     
+    lines = f.read().splitlines()
+    # store last line as a dictionary
+    first_line = lines[0].split(',')
+    last_line = lines[-1].split(',')
+    last_line_dict = dict(list(zip(first_line, last_line)))
+    # extract timestamp from last line
+    start_date = last_line_dict['timestamp']
+# convert timestamp from ISO-8601 to UNIX format
+start_date_dt = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+start_date_tuple = start_date_dt.timetuple()
+start_date_unix = int(time.mktime(start_date_tuple))
+
+# store URL for activities endpoint
+base_url = "https://www.strava.com/api/v3/"
+endpoint = "athlete/activities"
+url = base_url + endpoint
+# define headers and parameters for request
+headers = {"Authorization": "Bearer {}".format(access_token)}
+params = {"after": start_date_unix}
+# make GET request to Strava API
+req = requests.get(url, headers = headers, params = params).json()
 
 
-def touch(file):
-    """Create file without file content."""
-    with open(os.path.join(path, str(file)), "a"):
-        os.utime(path, None)
-
-
-if __name__ == "__main__":
-    for x in range(1, 10):
-        print("create file {}".format(x))
-        touch("{}.txt".format(x))
-
-    time.sleep(5)
